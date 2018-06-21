0dab7cd40d8cce8ddfeda2bcd485c0580ad44c76
x007xyz <1026161550@qq.com>
2018-06-21 09:45:52 +0800

    添加MapAutoComplete组件

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,61 +1,45 @@
 <template>
   <card>
-  <i-form :model="form" label-position="top" :rules="rules" inline>
-    <row :gutter="16">
-      <i-col :lg="6" :md="8" :sm="12" :xs="24">
-        <form-item label="测试text" prop="input">
-            <i-input v-model="form.input" type="text" placeholder="测试text" ></i-input>
-        </form-item>
-      </i-col>
-      <i-col :lg="6" :md="8" :sm="12" :xs="24">
-        <form-item label="测试text" prop="input2">
-            <i-input v-model="form.input2" type="text" placeholder="测试text" ></i-input>
-        </form-item>
-      </i-col>
-    </row>
-    <form-item>
-        <i-button type="primary" @click="submit">Submit</i-button>
-        <i-button type="ghost" style="margin-left: 8px">Cancel</i-button>
-    </form-item>
-  </i-form>
-  <span v-for="(item, index) in list" :key="index">
-    {{item}}
-  </span>
-</card>
+    <i-form ref="filter" :model="filter" inline>
+  <form-item prop="input">
+    <i-input v-model="filter.input" type="text" placeholder="测试text" ></i-input>
+  </form-item>
+
+  <form-item>
+    <i-button type="primary">查询</i-button>
+    <i-button type="error">删除所选</i-button>
+  </form-item>
+</i-form>
+<i-table :columns="columns" :data="list" size="small" ref="table"></i-table>
+<div style="overflow: hidden;padding-top: 10px;height: 40px;padding-right: 4px;">
+  <div style="float:right;">
+    <Page @on-change="changePage" :total="total" size="small" show-elevator show-sizer></Page>
+  </div>
+</div>
+  </card>
 </template>
 <script>
+import mixin from '@/mixins/list'
 export default {
+  mixins: [mixin],
   data () {
     return {
-      list: [],
-      form: {
-        input: '', input2: ''
+      url: '测试text',
+      filter: {
+        input: ''
       },
-      rules: {
-        input: [{
-          required: true,
-          message: 'The input cannot be empty',
-          trigger: 'blur'
-        }],
-        input2: [{
-          required: true,
-          message: 'The input cannot be empty',
-          trigger: 'blur'
-        }]
-
-      }
+      columns: [
+        {
+          type: 'selection',
+          width: 60,
+          align: 'center'
+        },
+        {
+          title: '测试text',
+          key: 'input'
+        }
+      ]
     }
-  },
-  methods: {
-    submit () {
-      this.list.push({name: 'q'})
-      this.list.push({name: 'q2'})
-    }
-  },
-  beforeMount () {
-    // this.list.push({name: 'q'})
-    // this.list.push({name: 'q2'})
-    // this.list = ['q', '2']
   }
 }
 </script>
