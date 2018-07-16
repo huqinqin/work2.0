3fa0f2bbf02f429773eb9ae302c8cbe5f76e7364
x007xyz <1026161550@qq.com>
2018-07-16 17:39:45 +0800

    mock依赖优化

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,14 +1,16 @@
 import Mock from 'mockjs'
-import { login, logout, getUserInfo } from './login'
+
+import { mapMock, mockSave, mockDelete } from './utils'
+
+import login from './login'
+import personnel from './personnel'
+import product from './product'
 
 import { getInstallerList, getInstallerReviewList, getInstallerAccountList, getInstallerDetail } from './installer'
-import { getProductAttributeList, getProductBrandList, getProductAttributeDetail, getProductList, getProductBrandDetail, getProductTrashList, getProductTrashRevert } from './product'
+// import { getProductAttributeList, getProductBrandList, getProductAttributeDetail, getProductList, getProductBrandDetail, getProductTrashList, getProductTrashRevert } from './product'
 import { getShopList, getShopDetail, shopEdit } from './shop'
-import { getPersonnelList, getPersonnelDetail } from './personnel'
 import { getCertList, getCertDetail } from './cert'
 // import { getActivityList } from './activity'
-import { mockDelete } from './delete'
-import { mockSave } from './save'
 
 const options = Mock.mock({
   'data|9': [
@@ -18,6 +20,10 @@ const options = Mock.mock({
     }
   ]
 })
+
+mapMock('/personnel', personnel)
+mapMock('/user', login)
+mapMock('/product', product)
 
 Mock.mock(/\/common\/getCodeTable/, 'post', req => {
   const data = JSON.parse(req.body)
@@ -30,28 +36,22 @@ Mock.mock(/\/common\/getCodeTable/, 'post', req => {
   }
 })
 // 登录相关和获取用户信息
-Mock.mock(/\/user\/login/, 'post', login)
-Mock.mock(/\/user\/logout/, 'post', logout)
-Mock.mock(/\/user\/infos/, 'post', getUserInfo)
 Mock.mock(/\/installer\/account\/list/, 'post', getInstallerAccountList)
 Mock.mock(/\/store\/list/, 'post', getInstallerReviewList)
 Mock.mock(/\/store\/get/, 'post', getInstallerDetail)
 Mock.mock(/\/installer\/getInstallerList/, 'post', getInstallerList)
-Mock.mock(/\/product\/attribute\/list/, 'post', getProductAttributeList)
-Mock.mock(/\/product\/attribute\/get/, 'post', getProductAttributeDetail)
-Mock.mock(/\/product\/brand\/get/, 'post', getProductBrandDetail)
-Mock.mock(/\/product\/brand\/list/, 'post', getProductBrandList)
-Mock.mock(/\/product\/brand\/del/, 'post', mockDelete)
-Mock.mock(/\/product\/brand\/save/, 'post', mockSave)
-Mock.mock(/\/product\/trash\/list/, 'post', getProductTrashList)
-Mock.mock(/\/product\/trash\/revert/, 'post', getProductTrashRevert)
-Mock.mock(/\/product/, 'post', getProductList)
+// Mock.mock(/\/product\/attribute\/list/, 'post', getProductAttributeList)
+// Mock.mock(/\/product\/attribute\/get/, 'post', getProductAttributeDetail)
+// Mock.mock(/\/product\/brand\/get/, 'post', getProductBrandDetail)
+// Mock.mock(/\/product\/brand\/list/, 'post', getProductBrandList)
+// Mock.mock(/\/product\/brand\/del/, 'post', mockDelete)
+// Mock.mock(/\/product\/brand\/save/, 'post', mockSave)
+// Mock.mock(/\/product\/trash\/list/, 'post', getProductTrashList)
+// Mock.mock(/\/product\/trash\/revert/, 'post', getProductTrashRevert)
+// Mock.mock(/\/product/, 'post', getProductList)
 Mock.mock(/\/shop\/list/, 'post', getShopList)
 Mock.mock(/\/shop\/get/, 'post', getShopDetail)
 Mock.mock(/\/shop\/save/, 'post', shopEdit)
-Mock.mock(/\/personnel\/list/, 'post', getPersonnelList)
-Mock.mock(/\/personnel\/get/, 'post', getPersonnelDetail)
-Mock.mock(/\/personnel\/save/, 'post', mockSave)
 // Mock.mock(/\/config\/promo\/list/, 'post', getActivityList)
 Mock.mock(/\/activity\/del/, 'post', mockDelete)
 Mock.mock(/\/cert\/list/, 'post', getCertList)
---
+++ src/mock/utils.js
@@ -0,0 +1,40 @@
+import Mock from 'mockjs'
+export const mapMock = (rootUrl, mockObj) => {
+  if (typeof mockObj === 'object') {
+    Object.keys(mockObj).forEach(item => {
+      const curUrl = `${rootUrl}/${item}`
+      const curOj = mockObj[item]
+      if (typeof curOj === 'object') {
+        mapMock(curUrl, curOj)
+      } else if (typeof curOj === 'function') {
+        Mock.mock(new RegExp(curUrl), 'post', curOj)
+      }
+    })
+  } else {
+    throw new Error('type error')
+  }
+}
+
+export const mockSave = req => {
+  const data = JSON.parse(req.body)
+  console.log('保存数据', data)
+  return {
+    'code': '000000',
+    'msg': '错误提示',
+    'stack': '错误栈',
+    'data': {
+      id: '1'
+    }
+  }
+}
+
+export const mockDelete = req => {
+  const data = JSON.parse(req.body)
+  console.log('删除数据', data)
+  return {
+    'code': '000000',
+    'msg': '错误提示',
+    'stack': '错误栈',
+    'data': null
+  }
+}
