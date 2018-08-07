31cd6e699824bb4caa3dbe98ef803cb5d9659402
x007xyz <1026161550@qq.com>
2018-08-07 15:38:08 +0800

    添加购物券路由与页面

---
+++ src/view/CouponNew.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="content">
+    New
+  </div>
+</template>
+<script>
+export default {
+  name: 'CouponNew',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style lang="css" scoped>
+</style>
