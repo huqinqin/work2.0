6e72e3543dc6c72e7c23560feae007570f4428e7
huqinqin <896268808@qq.com>
2018-09-13 20:58:50 +0800

    批量的把组件进行注册

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 const context = require.context('./', false, /\.vue$/)
 
+/* 批量的把组件进行注册 */
 context.keys().forEach(key => {
   const name = key.match(/([a-zA-Z0-9]*)\.vue$/i)[1]
   Vue.component(name, context(key).default)
